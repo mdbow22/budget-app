ddb50bd13e61abfae811d63cdb08edd35778a246
Matthew Bow <mdbow22@gmail.com>
2023-08-07 14:39:54 -0400

    initial setup/bootstrapping

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,5 +5,30 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#649C64",
+
+          secondary: "#A39171",
+
+          accent: "#6D4C3D",
+
+          neutral: "#F0EAE6",
+
+          "base-100": "#F0EAE6",
+
+          info: "#A1CEC6",
+
+          success: "#36d399",
+
+          warning: "#fbbd23",
+
+          error: "#f87272",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
 } satisfies Config;
