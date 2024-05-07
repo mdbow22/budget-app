3730333e164f803ae1504f907a9bde16c1415a0d
mdbow22 <mdbow22@gmail.com>
2024-05-07 15:47:18 -0400

    backend and historical data

--- src/modules/accountPage/TransactionRow.tsx
+++ src/modules/accountPage/TransactionRow.tsx
@@ -11,10 +11,11 @@ export type TransactionRowProps = {
     PayorPayee: PayorPayee | null;
     Category: Category | null;
   };
-  setTransToDel: React.Dispatch<
+  setTransToDel?: React.Dispatch<
     React.SetStateAction<DeleteTransaction | undefined>
   >;
-  modalRef: React.RefObject<HTMLDialogElement>;
+  modalRef?: React.RefObject<HTMLDialogElement>;
+  noedit?: boolean;
 };
 
 export type EditTransFormType = {
@@ -30,6 +31,7 @@ const TransactionRow: React.FC<TransactionRowProps> = ({
   trans,
   setTransToDel,
   modalRef,
+  noedit,
 }) => {
   const [isEdit, setIsEdit] = useState<number>();
   const [submitted, setSubmitted] = useState(false);
@@ -128,183 +130,189 @@ const TransactionRow: React.FC<TransactionRowProps> = ({
 
   return (
     <tr className="hover" key={`trans-${trans.id}`}>
-      {loading ?
-        <td colSpan={6} className='border text-center'>
-          <div className='text-center loading loading-dots loading-sm'></div>
+      {loading ? (
+        <td colSpan={6} className="border text-center">
+          <div className="loading loading-dots loading-sm text-center"></div>
         </td>
-        :
+      ) : (
         <>
-      <td>
-        {!!isEdit ? (
-          <input
-            type="date"
-            id="date"
-            className={`input input-bordered input-xs ${
-              submitted && !editedTrans.date ? "input-error" : ""
-            }`}
-            value={
-              editedTrans.date
-                ? DateTime.fromJSDate(editedTrans.date).toFormat("yyyy-MM-dd")
-                : undefined
-            }
-            onChange={(e) =>
-              dispatch({
-                type: "setField",
-                payload: { field: "date", value: e.target.value },
-              })
-            }
-          />
-        ) : (
-          DateTime.fromJSDate(trans.date).toFormat("MM/dd/yyyy")
-        )}
-      </td>
-      <td>
-        {!!isEdit ? (
-          <>
-            <input
-              type="text"
-              id="category"
-              list="categories"
-              className={`select select-bordered select-xs`}
-              value={editedTrans.category}
-              onChange={(e) =>
-                dispatch({
-                  type: "setField",
-                  payload: { field: "category", value: e.target.value },
-                })
-              }
-            />
-            <datalist id="categories">
-              {categories
-                ?.filter((cat) =>
-                  editedTrans.amount > 0
-                    ? cat.type === "credit"
-                    : cat.type === "debit"
-                )
-                .map((cat) => {
-                  return <option value={cat.name} key={`cat-${cat.id}`} />;
-                })}
-            </datalist>
-          </>
-        ) : (
-          trans.Category?.name
-        )}
-      </td>
-      <td>
-        {!!isEdit ? (
-          <input
-            type="text"
-            id="description"
-            className={`input input-bordered input-xs`}
-            value={editedTrans.description ?? undefined}
-            onChange={(e) =>
-              dispatch({
-                type: "setField",
-                payload: { field: "description", value: e.target.value },
-              })
-            }
-          />
-        ) : (
-          trans.description
-        )}
-      </td>
-      <td>
-        {!!isEdit ? (
-          <>
-            <input
-              type="text"
-              id="payorPayee"
-              list="thirdParties"
-              className={`select select-bordered select-xs`}
-              value={editedTrans.thirdParty}
-              onChange={(e) =>
-                dispatch({
-                  type: "setField",
-                  payload: { field: "thirdParty", value: e.target.value },
-                })
-              }
-            />
-            <datalist id="thirdParties">
-              {!partiesLoading &&
-                thirdParties?.map((party) => {
-                  return (
-                    <option
-                      value={party.thirdparty}
-                      key={`party-${party.id}`}
-                    />
-                  );
-                })}
-            </datalist>
-          </>
-        ) : (
-          trans.PayorPayee?.thirdparty
-        )}
-      </td>
-      <td className="text-right">
-        {!!isEdit ? (
-          <input
-            type="test"
-            inputMode="decimal"
-            className={`input input-bordered input-xs ${
-              submitted && (editedTrans.amount === 0 || !editedTrans.amount)
-                ? "input-error"
-                : ""
-            } ${loading && 'loading loading-dots'}`}
-            value={editedTrans.amount}
-            onChange={(e) =>
-              dispatch({
-                type: "setField",
-                payload: { field: "amount", value: e.target.value },
-              })
-            }
-            pattern="\d+\.?(\d{1,2})?"
-          />
-        ) : !trans.amount.toString().includes(".") ? (
-          trans.amount.toString().concat(".00")
-        ) : (
-          trans.amount
-        )}
-      </td>
-      <td className="text-right">
-        {isEdit !== trans.id && (
-          <div className="tooltip tooltip-accent" data-tip="Edit">
-            <button
-              onClick={() => {
-                setIsEdit(trans.id);
-                dispatch({ type: "initialize", payload: null });
-              }}
-            >
-              <Pencil size={18} />
-            </button>
-          </div>
-        )}
-        {isEdit === trans.id && (
-          <div className="tooltip tooltip-accent" data-tip="Save">
-            <button onClick={() => updateTransaction()}>
-              <Save size={18} />
-            </button>
-          </div>
-        )}
-        <div className="tooltip tooltip-accent" data-tip="Delete">
-          <button
-            onClick={() => {
-              setTransToDel({
-                id: trans.id,
-                amount: trans.amount,
-                payorPayee: trans.PayorPayee?.thirdparty,
-                date: DateTime.fromJSDate(trans.date).toFormat("MM/dd/yyyy"),
-                isTransfer: trans.isTransfer,
-              });
-              modalRef.current?.showModal();
-            }}
-            className="pl-3"
-          >
-            <X size={18} />
-          </button>
-        </div>
-      </td>
-      </>
-    }
+          <td>
+            {!!isEdit ? (
+              <input
+                type="date"
+                id="date"
+                className={`input input-bordered input-xs ${
+                  submitted && !editedTrans.date ? "input-error" : ""
+                }`}
+                value={
+                  editedTrans.date
+                    ? DateTime.fromJSDate(editedTrans.date).toFormat(
+                        "yyyy-MM-dd"
+                      )
+                    : undefined
+                }
+                onChange={(e) =>
+                  dispatch({
+                    type: "setField",
+                    payload: { field: "date", value: e.target.value },
+                  })
+                }
+              />
+            ) : (
+              DateTime.fromJSDate(trans.date).toFormat("MM/dd/yyyy")
+            )}
+          </td>
+          <td>
+            {!!isEdit ? (
+              <>
+                <input
+                  type="text"
+                  id="category"
+                  list="categories"
+                  className={`select select-bordered select-xs`}
+                  value={editedTrans.category}
+                  onChange={(e) =>
+                    dispatch({
+                      type: "setField",
+                      payload: { field: "category", value: e.target.value },
+                    })
+                  }
+                />
+                <datalist id="categories">
+                  {categories
+                    ?.filter((cat) =>
+                      editedTrans.amount > 0
+                        ? cat.type === "credit"
+                        : cat.type === "debit"
+                    )
+                    .map((cat) => {
+                      return <option value={cat.name} key={`cat-${cat.id}`} />;
+                    })}
+                </datalist>
+              </>
+            ) : (
+              trans.Category?.name
+            )}
+          </td>
+          <td>
+            {!!isEdit ? (
+              <input
+                type="text"
+                id="description"
+                className={`input input-bordered input-xs`}
+                value={editedTrans.description ?? undefined}
+                onChange={(e) =>
+                  dispatch({
+                    type: "setField",
+                    payload: { field: "description", value: e.target.value },
+                  })
+                }
+              />
+            ) : (
+              trans.description
+            )}
+          </td>
+          <td>
+            {!!isEdit ? (
+              <>
+                <input
+                  type="text"
+                  id="payorPayee"
+                  list="thirdParties"
+                  className={`select select-bordered select-xs`}
+                  value={editedTrans.thirdParty}
+                  onChange={(e) =>
+                    dispatch({
+                      type: "setField",
+                      payload: { field: "thirdParty", value: e.target.value },
+                    })
+                  }
+                />
+                <datalist id="thirdParties">
+                  {!partiesLoading &&
+                    thirdParties?.map((party) => {
+                      return (
+                        <option
+                          value={party.thirdparty}
+                          key={`party-${party.id}`}
+                        />
+                      );
+                    })}
+                </datalist>
+              </>
+            ) : (
+              trans.PayorPayee?.thirdparty
+            )}
+          </td>
+          <td className="text-right">
+            {!!isEdit ? (
+              <input
+                type="test"
+                inputMode="decimal"
+                className={`input input-bordered input-xs ${
+                  submitted && (editedTrans.amount === 0 || !editedTrans.amount)
+                    ? "input-error"
+                    : ""
+                } ${loading && "loading loading-dots"}`}
+                value={editedTrans.amount}
+                onChange={(e) =>
+                  dispatch({
+                    type: "setField",
+                    payload: { field: "amount", value: e.target.value },
+                  })
+                }
+                pattern="\d+\.?(\d{1,2})?"
+              />
+            ) : !trans.amount.toString().includes(".") ? (
+              trans.amount.toString().concat(".00")
+            ) : (
+              trans.amount
+            )}
+          </td>
+          <td className="text-right">
+            {isEdit !== trans.id && !noedit && (
+              <div className="tooltip tooltip-accent" data-tip="Edit">
+                <button
+                  onClick={() => {
+                    setIsEdit(trans.id);
+                    dispatch({ type: "initialize", payload: null });
+                  }}
+                >
+                  <Pencil size={18} />
+                </button>
+              </div>
+            )}
+            {isEdit === trans.id && (
+              <div className="tooltip tooltip-accent" data-tip="Save">
+                <button onClick={() => updateTransaction()}>
+                  <Save size={18} />
+                </button>
+              </div>
+            )}
+            {!noedit && setTransToDel && modalRef && (
+              <div className="tooltip tooltip-accent" data-tip="Delete">
+                <button
+                  onClick={() => {
+                    setTransToDel({
+                      id: trans.id,
+                      amount: trans.amount,
+                      payorPayee: trans.PayorPayee?.thirdparty,
+                      date: DateTime.fromJSDate(trans.date).toFormat(
+                        "MM/dd/yyyy"
+                      ),
+                      isTransfer: trans.isTransfer,
+                    });
+                    modalRef.current?.showModal();
+                  }}
+                  className="pl-3"
+                >
+                  <X size={18} />
+                </button>
+              </div>
+            )}
+          </td>
+        </>
+      )}
     </tr>
   );
 };
--- src/pages/budgets/[id].tsx
+++ src/pages/budgets/[id].tsx
@@ -1,21 +1,197 @@
-import React from 'react'
-import { NextPageWithLayout } from '../_app'
-import Layout from '~/modules/layouts/Layout';
+import React, { useMemo, useState } from "react";
+import { NextPageWithLayout } from "../_app";
+import Layout from "~/modules/layouts/Layout";
+import { api } from "~/utils/api";
+import { useRouter } from "next/router";
+import TransactionRow from "~/modules/accountPage/TransactionRow";
+import { DateTime } from "luxon";
+import { Budget } from "@prisma/client";
+import { ReturnArray } from "~/server/api/routers/budgets";
+
+export type Transactions = {
+  transactions: ReturnArray[];
+  start: Date;
+  end: Date;
+};
 
 const BudgetPage: NextPageWithLayout = () => {
+  const [period, setPeriod] = useState<Transactions | undefined>();
+  const { query } = useRouter();
+  const { data } = api.budgets.getBudgetInfo.useQuery(
+    {
+      id: parseInt(query.id as string),
+      cursor: 1,
+      perPage: 25,
+    },
+    {
+      enabled: !!query.id,
+      keepPreviousData: true,
+      select: (d) => {
+        if (!period) {
+          setPeriod(d?.pastPeriods[0]);
+        }
+        return d;
+      },
+    }
+  );
+
+  const periodString = (budget: Budget) => {
+    const now = DateTime.now();
+    let start = "";
+    let end = "";
+    switch (budget.reset) {
+      case "weekly": {
+        start = now.startOf("week").toFormat("MM/dd");
+        end = now.endOf("week").toFormat("MM/dd");
+        break;
+      }
+      case "monthly": {
+        start = now.startOf("month").toFormat("MM/dd");
+        end = now.endOf("month").toFormat("MM/dd");
+        break;
+      }
+      case "quarterly": {
+        start = now.startOf("quarter").toFormat("MM/dd");
+        end = now.endOf("quarter").toFormat("MM/dd");
+        break;
+      }
+      case "annually": {
+        start = now.startOf("year").toFormat("MM/dd");
+        end = now.endOf("year").toFormat("MM/dd");
+        break;
+      }
+      case "biannually": {
+        if (now.get("month") > 6) {
+          const newMonth = now.set({ month: 7 });
+          start = newMonth.startOf("month").toFormat("MM/dd");
+          end = now.endOf("year").toFormat("MM/dd");
+        } else {
+          const newMonth = now.set({ month: 6 });
+          start = now.startOf("year").toFormat("MM/dd");
+          end = newMonth.endOf("quarter").toFormat("MM/dd");
+        }
+        break;
+      }
+    }
+
+    return `${start} - ${end}`;
+  };
+
+  const totalSpend = useMemo(() => {
+    return data && period
+      ? Math.abs(
+          Math.floor(
+            period.transactions.reduce((a, b) => a + b.amount, 0) * 100
+          ) / 100
+        )
+      : 0;
+  }, [data, period]);
+
+  const calculateBiggestCat = (period: Transactions) => {
+    let places: { [x: string]: number } = {};
+    const payorPayees = [
+      ...new Set(period.transactions.map((t) => t.PayorPayee?.thirdparty)),
+    ];
+    payorPayees.forEach((pp) => {
+      if (pp && !places[pp]) {
+        places[pp] = Math.abs(
+          Math.floor(
+            period.transactions
+              .filter((t) => t.PayorPayee?.thirdparty === pp)
+              .reduce((a, b) => a + b.amount, 0) * 100
+          ) / 100
+        );
+      }
+    });
+
+    let largest = "";
+    Object.keys(places).forEach((pp) => {
+      if (
+        (places[pp] && places[largest] && places[pp] > places[largest]!) ||
+        !largest.length
+      ) {
+        largest = pp;
+      }
+    });
+
+    return { largest, amount: places[largest] };
+  };
+
   return (
-    <div>[id]</div>
-  )
-}
+    <>
+      {data && period && (
+        <div className="p-5">
+          <h1 className="text-2xl font-bold">{data?.budget.name} - <span className="text-xl">${data.budget.max} {data.budget.reset} spend allowed</span></h1>
+          <h2 className="mt-5 text-xl font-bold">
+            Transactions For Period{" "}
+            <select
+              className="cursor-pointer bg-transparent"
+              value={DateTime.fromJSDate(period.start).toFormat("MM/dd/yyyy")}
+              onChange={(e) => {
+                const newPeriod = data.pastPeriods.find((p) => {
+                  return (
+                    `${DateTime.fromJSDate(p.start).toFormat("MM/dd/yyyy")}` ===
+                    e.target.value
+                  );
+                });
+                setPeriod(newPeriod);
+              }}
+            >
+              {data.pastPeriods.map((p) => {
+                return (
+                  <option
+                    value={DateTime.fromJSDate(p.start).toFormat("MM/dd/yyyy")}
+                  >
+                    {DateTime.fromJSDate(p.start).toFormat("MM/dd")} -{" "}
+                    {DateTime.fromJSDate(p.end).toFormat("MM/dd")}
+                  </option>
+                );
+              })}
+            </select>
+          </h2>
+          <div className="mt-3 grid grid-cols-1 gap-3 md:grid-cols-2">
+            <table className="table table-zebra table-sm">
+              <thead>
+                <tr className="text-center text-primary">
+                  <th>Date</th>
+                  <th>Category</th>
+                  <th>Description</th>
+                  <th>Payor/Payee</th>
+                  <th>Amount</th>
+                </tr>
+              </thead>
+              <tbody className="border border-base-200">
+                {period?.transactions.map((t) => {
+                  return <TransactionRow noedit trans={t} />;
+                })}
+              </tbody>
+            </table>
+            <div>
+              <p>
+                Total Spend: ${totalSpend} (
+                {Math.floor((totalSpend / data.budget.max) * 100)}% of max)
+              </p>
+              <p>
+                Most Spent at:{" "}
+                {calculateBiggestCat(period).largest} ($
+                {calculateBiggestCat(period).amount})
+              </p>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
 
 BudgetPage.getLayout = (page: React.ReactNode) => {
-    return (
-      <>
-        <Layout>{page}</Layout>
-      </>
-    );
-  };
-  
-  BudgetPage.auth = true;
+  return (
+    <>
+      <Layout>{page}</Layout>
+    </>
+  );
+};
 
-export default BudgetPage
+BudgetPage.auth = true;
+
+export default BudgetPage;
