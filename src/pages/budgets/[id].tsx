35dcf7d2811e10a515584bbd3c52420b9b77cb7b
mdbow22 <mdbow22@gmail.com>
2024-05-17 15:03:11 -0400

    filter for budget to see by vendor

--- src/pages/budgets/[id].tsx
+++ src/pages/budgets/[id].tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { NextPageWithLayout } from "../_app";
 import Layout from "~/modules/layouts/Layout";
 import { api } from "~/utils/api";
@@ -7,15 +7,24 @@ import TransactionRow from "~/modules/accountPage/TransactionRow";
 import { DateTime } from "luxon";
 import { Budget } from "@prisma/client";
 import { ReturnArray } from "~/server/api/routers/budgets";
+import { Filter } from "../../../node_modules/lucide-react";
+
+export interface TransArray extends ReturnArray {
+  thirdparty: string | undefined;
+}
 
 export type Transactions = {
-  transactions: ReturnArray[];
+  transactions: TransArray[];
   start: Date;
   end: Date;
 };
 
 const BudgetPage: NextPageWithLayout = () => {
   const [period, setPeriod] = useState<Transactions | undefined>();
+  const [filters, setFilters] = useState<
+    { column: string; value: string | number }[]
+  >([]);
+  const [filterOptions, setFilterOptions] = useState<string[]>([]);
   const { query } = useRouter();
   const { data } = api.budgets.getBudgetInfo.useQuery(
     {
@@ -27,55 +36,34 @@ const BudgetPage: NextPageWithLayout = () => {
       enabled: !!query.id,
       keepPreviousData: true,
       select: (d) => {
+        const newData = {
+          ...d,
+          pastPeriods: d?.pastPeriods.map((p) => ({
+            ...p,
+            transactions: p.transactions.map((t) => ({
+              ...t,
+              thirdparty: t.PayorPayee?.thirdparty,
+            })),
+          })),
+        };
         if (!period) {
-          setPeriod(d?.pastPeriods[0]);
+          setPeriod(newData?.pastPeriods?.[0]);
         }
-        return d;
+        if (!filterOptions.length) {
+          setFilterOptions(
+            [
+              ...new Set(
+                newData?.pastPeriods?.[0]?.transactions
+                  .map((t) => t.PayorPayee?.thirdparty)
+                  .filter((t) => t !== null && t !== undefined)
+              ),
+            ].sort((a, b) => a.localeCompare(b))
+          );
+        }
+        return newData;
       },
     }
   );
-
-  const periodString = (budget: Budget) => {
-    const now = DateTime.now();
-    let start = "";
-    let end = "";
-    switch (budget.reset) {
-      case "weekly": {
-        start = now.startOf("week").toFormat("MM/dd");
-        end = now.endOf("week").toFormat("MM/dd");
-        break;
-      }
-      case "monthly": {
-        start = now.startOf("month").toFormat("MM/dd");
-        end = now.endOf("month").toFormat("MM/dd");
-        break;
-      }
-      case "quarterly": {
-        start = now.startOf("quarter").toFormat("MM/dd");
-        end = now.endOf("quarter").toFormat("MM/dd");
-        break;
-      }
-      case "annually": {
-        start = now.startOf("year").toFormat("MM/dd");
-        end = now.endOf("year").toFormat("MM/dd");
-        break;
-      }
-      case "biannually": {
-        if (now.get("month") > 6) {
-          const newMonth = now.set({ month: 7 });
-          start = newMonth.startOf("month").toFormat("MM/dd");
-          end = now.endOf("year").toFormat("MM/dd");
-        } else {
-          const newMonth = now.set({ month: 6 });
-          start = now.startOf("year").toFormat("MM/dd");
-          end = newMonth.endOf("quarter").toFormat("MM/dd");
-        }
-        break;
-      }
-    }
-
-    return `${start} - ${end}`;
-  };
 
   const totalSpend = useMemo(() => {
     return data && period
@@ -117,27 +105,88 @@ const BudgetPage: NextPageWithLayout = () => {
     return { largest, amount: places[largest] };
   };
 
+  useEffect(() => {
+    if (filters.length) {
+      setPeriod((prev) => {
+        const foundPeriod = data?.pastPeriods?.find(
+          (f) =>
+            prev?.start &&
+            prev?.end &&
+            DateTime.fromJSDate(f.start).equals(
+              DateTime.fromJSDate(prev?.start)
+            )
+        );
+        const transactions = foundPeriod?.transactions.filter((t) => {
+          let keep = false;
+          filters.forEach((f) => {
+            if (f.value) {
+              console.log("filter value: ", f.value);
+              console.log("transaction: ", t[f.column as keyof TransArray]);
+              keep = f.value === t[f.column as keyof TransArray];
+            }
+          });
+          return keep;
+        });
+        console.log(transactions);
+        return prev
+          ? {
+              ...prev,
+              transactions: transactions ?? prev.transactions,
+            }
+          : undefined;
+      });
+    } else {
+      setPeriod((prev) =>
+        prev
+          ? data?.pastPeriods?.find(
+              (f) =>
+                prev?.start &&
+                prev?.end &&
+                DateTime.fromJSDate(f.start).equals(
+                  DateTime.fromJSDate(prev?.start)
+                )
+            )
+          : undefined
+      );
+    }
+  }, [filters, setPeriod]);
+
   return (
     <>
       {data && period && (
         <div className="p-5">
-          <h1 className="text-2xl font-bold">{data?.budget.name} - <span className="text-xl">${data.budget.max} {data.budget.reset} spend allowed</span></h1>
+          <h1 className="text-2xl font-bold">
+            {data?.budget?.name} -{" "}
+            <span className="text-xl">
+              ${data.budget?.max} {data.budget?.reset} spend allowed
+            </span>
+          </h1>
           <h2 className="mt-5 text-xl font-bold">
             Transactions For Period{" "}
             <select
               className="cursor-pointer bg-transparent"
               value={DateTime.fromJSDate(period.start).toFormat("MM/dd/yyyy")}
               onChange={(e) => {
-                const newPeriod = data.pastPeriods.find((p) => {
+                const newPeriod = data.pastPeriods?.find((p) => {
                   return (
                     `${DateTime.fromJSDate(p.start).toFormat("MM/dd/yyyy")}` ===
                     e.target.value
                   );
                 });
                 setPeriod(newPeriod);
+                setFilters([]);
+                setFilterOptions(
+                  [
+                    ...new Set(
+                      newPeriod?.transactions
+                        .map((t) => t.PayorPayee?.thirdparty)
+                        .filter((t) => t !== null && t !== undefined)
+                    ),
+                  ].sort((a, b) => a.localeCompare(b))
+                );
               }}
             >
-              {data.pastPeriods.map((p) => {
+              {data.pastPeriods?.map((p) => {
                 return (
                   <option
                     value={DateTime.fromJSDate(p.start).toFormat("MM/dd/yyyy")}
@@ -149,6 +198,92 @@ const BudgetPage: NextPageWithLayout = () => {
               })}
             </select>
           </h2>
+          <div className="flex w-full flex-row items-end gap-3">
+            <div className="pt-2">
+              <Filter size={20} />
+            </div>
+            {/* <div className="form-control">
+              <label className="label">
+                <span className="label-text">Description</span>
+              </label>
+              <select
+                className="select select-bordered select-xs"
+                value={
+                  filters?.find((f) => f.column === "description")?.value ?? "0"
+                }
+                onChange={(e) =>
+                  setFilters((prev) => {
+                   const filtered = prev.filter(i => i.column !== 'description')
+                    filtered.push({ column: "description", value: e.target.value });
+                    return filtered;
+                })
+                }
+              >
+                <option disabled value="0">
+                  Select one...
+                </option>
+                {[
+                  ...new Set(
+                    period.transactions
+                      .map((t) => t.description)
+                      .filter((t) => t !== null)
+                  ),
+                ]
+                  .sort((a, b) => a.localeCompare(b))
+                  .map((cat) => {
+                    return (
+                      <option key={cat} value={cat}>
+                        {cat}
+                      </option>
+                    );
+                  })}
+              </select>
+            </div> */}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Payor/Payee</span>
+              </label>
+              <select
+                className="select select-bordered select-xs"
+                value={
+                  filters?.find((f) => f.column === "thirdparty")?.value ?? "0"
+                }
+                onChange={(e) =>
+                  setFilters((prev) => {
+                    const filtered = prev.filter(
+                      (i) => i.column !== "thirdparty"
+                    );
+                    filtered.push({
+                      column: "thirdparty",
+                      value: e.target.value,
+                    });
+                    return filtered;
+                  })
+                }
+              >
+                <option disabled value="0">
+                  Select one...
+                </option>
+                {filterOptions.map((cat) => {
+                  return (
+                    <option key={cat} value={cat}>
+                      {cat}
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
+            <div>
+              <div className="">&nbsp;</div>
+              <button
+                className="btn btn-xs"
+                type="button"
+                onClick={() => setFilters([])}
+              >
+                Clear
+              </button>
+            </div>
+          </div>
           <div className="mt-3 grid grid-cols-1 gap-3 md:grid-cols-2">
             <table className="table table-zebra table-sm">
               <thead>
@@ -161,19 +296,33 @@ const BudgetPage: NextPageWithLayout = () => {
                 </tr>
               </thead>
               <tbody className="border border-base-200">
-                {period?.transactions.map((t) => {
-                  return <TransactionRow noedit trans={t} />;
-                })}
+                {period?.transactions
+                  .sort((a, b) =>
+                    DateTime.fromJSDate(a.date) < DateTime.fromJSDate(b.date)
+                      ? 1
+                      : -1
+                  )
+                  .map((t) => {
+                    return <TransactionRow noedit trans={t} />;
+                  })}
+                <tr>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td className="text-right">{totalSpend}</td>
+                </tr>
               </tbody>
             </table>
             <div>
               <p>
                 Total Spend: ${totalSpend} (
-                {Math.floor((totalSpend / data.budget.max) * 100)}% of max)
+                {!!data.budget?.max &&
+                  Math.floor((totalSpend / data.budget?.max) * 100)}
+                % of max)
               </p>
               <p>
-                Most Spent at:{" "}
-                {calculateBiggestCat(period).largest} ($
+                Most Spent at: {calculateBiggestCat(period).largest} ($
                 {calculateBiggestCat(period).amount})
               </p>
             </div>
