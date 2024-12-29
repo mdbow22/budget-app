602e0caf6853c9d6edb630e1e2c944f6aff376f4
Matthew Bow <mdbow22@gmail.com>
2024-12-28 23:39:16 -0500

    way shorter number to currency conversion

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,45 +1,22 @@
 export const formatCurrency = (rawBal: number) => {
-    if (rawBal === 0) {
-      return "$0.00";
-    }
+  if (rawBal === 0) {
+    return "$0.00";
+  }
 
-    const stringBal = Math.floor(Math.abs(rawBal)).toString();
-    console.log(stringBal);
-    const reversedBal = stringBal.includes(".")
-      ? stringBal
-          .substring(
-            0,
-            rawBal
-              .toString()
-              .split("")
-              .findIndex((d) => d === ".") ?? rawBal.toString().length
-          )
-          .split("")
-          .reverse()
-      : stringBal.split("").reverse();
-    const negative = rawBal < 0;
-    let cents = rawBal.toString().includes(".")
-      ? rawBal.toString().substring(
-          rawBal
-            .toString()
-            .split("")
-            .findIndex((e) => e === ".")
-        )
-      : ".00";
-    
-    if(cents.length < 3) {
-        cents = cents + '0'
-    }
-
-    const formattedBal = reversedBal
-      .map((digit, index) => {
-        if ((index + 1) % 3 === 0 && reversedBal.length > 3) {
-          return `,${digit}`;
-        }
-        return digit;
-      })
-      .reverse()
-      .join("");
-
-    return negative ? `($${formattedBal}${cents})` : `$${formattedBal}${cents}`;
-  };
+  const stringBal = Math.abs(rawBal).toString();
+  let realBal = "$0.00";
+  if (Number.isInteger(rawBal)) {
+    realBal =
+      rawBal < 0
+        ? `(\$${stringBal.toString()}.00)`
+        : `\$${stringBal.toString()}.00`;
+  } else if (Number.isInteger(rawBal * 10)) {
+    realBal =
+      rawBal < 0
+        ? `(\$${stringBal.toString()}0)`
+        : `\$${stringBal.toString()}0`;
+  } else {
+    realBal = rawBal < 0 ? "($" + stringBal + ")" : "$" + stringBal;
+  }
+  return realBal;
+};
